ebe3df2ee6780abfef3b5e8f5c6ffcf13f7e286e
Alok Jha <alokjha0209@gmail.com>
2025-07-07 09:26:15 +0530

    implement pagination and sorting

--- src/main/java/com/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.movieflix.service;
 
 import com.movieflix.dto.MovieDto;
+import com.movieflix.dto.MoviePageResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,4 +19,7 @@ public interface MovieService {
 
     String deleteMovie(Integer movieId) throws IOException;
 
+    MoviePageResponse getAllMoviesWithPagination(Integer pageNumber, Integer pageSize);
+
+    MoviePageResponse getAllMoviesWithPaginationAndSorting(Integer pageNumber, Integer pageSize, String sortBy, String dir);
 }
