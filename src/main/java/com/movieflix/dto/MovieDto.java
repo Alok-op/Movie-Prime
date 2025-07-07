2685bdfa8aacc5f2e12cf4027fc8cfef4b65cf14
Alok Jha <alokjha0209@gmail.com>
2025-07-07 17:35:49 +0530

    Downgraded java version and spring security setup

--- src/main/java/com/movieflix/dto/MovieDto.java
+++ src/main/java/com/movieflix/dto/MovieDto.java
@@ -1,12 +1,14 @@
 package com.movieflix.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovieDto {
--- src/main/java/com/movieflix/entities/Movie.java
+++ src/main/java/com/movieflix/entities/Movie.java
@@ -2,10 +2,18 @@ package com.movieflix.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
 
     @Id
@@ -34,77 +42,4 @@ public class Movie {
     @Column(nullable = false)
     @NotBlank(message = "Please provide movie's poster!")
     private String poster;
-
-    // No-args constructor (required by JPA)
-    public Movie() {
-    }
-
-    // All-args constructor
-    public Movie(Integer movieId, String title, String director, String studio,
-                 Set<String> movieCast, Integer releaseYear, String poster) {
-        this.movieId = movieId;
-        this.title = title;
-        this.director = director;
-        this.studio = studio;
-        this.movieCast = movieCast;
-        this.releaseYear = releaseYear;
-        this.poster = poster;
-    }
-
-    // Getters and Setters
-    public Integer getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(Integer movieId) {
-        this.movieId = movieId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getStudio() {
-        return studio;
-    }
-
-    public void setStudio(String studio) {
-        this.studio = studio;
-    }
-
-    public Set<String> getMovieCast() {
-        return movieCast;
-    }
-
-    public void setMovieCast(Set<String> movieCast) {
-        this.movieCast = movieCast;
-    }
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
 }
