8aa01a6ee351135687811ff1715ce2372de412d8
Alok Jha <alokjha0209@gmail.com>
2025-07-08 22:37:18 +0530

    Created entities and repositories for authentication

---
+++ src/main/java/com/movieflix/auth/entities/User.java
@@ -0,0 +1,92 @@
+package com.movieflix.auth.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NotBlank(message = "This name field can't be blank")
+    private String name;
+
+    @NotBlank(message = "This username field can't be blank")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "This email field can't be blank")
+    @Column(unique = true)
+    @Email(message = "Please enter email in proper format")
+    private String email;
+
+    @NotBlank(message = "This password field can't be blank")
+    @Size(min = 5, message = "The password must")
+    private String password;
+
+    @OneToOne(mappedBy = "user")
+    private RefreshToken refreshToken;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private boolean isEnabled = true;
+
+    private boolean isCredentialsNonExpired = true;
+
+    private boolean isAccountNonLocked = true;
+
+    private boolean isAccountNonExpired = true;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
